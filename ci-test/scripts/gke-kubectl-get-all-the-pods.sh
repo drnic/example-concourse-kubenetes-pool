e32d7b259605b28d9cc4525784187ed6c1003049
Dr Nic Williams <drnicwilliams@gmail.com>
2018-11-13 06:51:57 +1000

    specify which cloud provider to use for fetching credentials

--- ci-test/scripts/gke-kubectl-get-all-the-pods.sh
+++ ci-test/scripts/gke-kubectl-get-all-the-pods.sh
@@ -7,11 +7,20 @@ set -eu
 
 gcloud auth activate-service-account --key-file <(echo "$GCP_SERVICE_ACCOUNT_JSON")
 
-PROJECT=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".project")
-CLUSTER_NAME=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_name")
-CLUSTER_ZONE=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_zone")
-# CLUSTER_REGION=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_region")
+CLOUD_PROVIDER=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".provider")
+case ${CLOUD_PROVIDER:?require "provider" key in pool resource metadata} in
+  google|gcp)
+    PROJECT=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".project")
+    CLUSTER_NAME=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_name")
+    CLUSTER_ZONE=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_zone")
+    # CLUSTER_REGION=$(cat $K8S_CLUSTER_METADATA/metadata | jq -r ".cluster_region")
 
-gcloud container clusters get-credentials $CLUSTER_NAME --region $CLUSTER_ZONE --project $PROJECT
+    gcloud container clusters get-credentials $CLUSTER_NAME --region $CLUSTER_ZONE --project $PROJECT
+    ;;
+  *)
+    echo "ERROR: unknown cloud provider $CLOUD_PROVIDER"
+    exit 1
+  ;;
+esac
 
 kubectl get pods --all-namespaces
